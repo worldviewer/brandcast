9c8ea8e9c8224f0d8af2ed0ea281c49453459c56
Chris Reeve <paradigmsareconstructed@gmail.com>
2017-04-05 19:01:10 -0700

    Scaffolded the 3 subcomponents

---
+++ src/Gallery/Gallery.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Gallery.scss';
+
+class Gallery extends Component {
+	render() {
+		return (
+			<div className="Gallery">
+
+			</div>
+		);
+	}
+}
+
+export default Gallery;
---
+++ src/Slider/Slider.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Slider.scss';
+
+class Slider extends Component {
+	render() {
+		return (
+			<div className="Slider">
+
+			</div>
+		);
+	}
+}
+
+export default Slider;
